b9b0a631de2d364013ba67828714a9a35794155b
goughjo02 <goughjo02@gmail.com>
2021-04-10 23:15:53 +0100

    add update track mutation

--- app/tracks/schema.py
+++ app/tracks/schema.py
@@ -4,9 +4,11 @@ from graphene_django import DjangoObjectType
 
 from .models import Track
 
+
 class TrackType(DjangoObjectType):
     class Meta:
         model = Track
+
 
 class Query(graphene.ObjectType):
     tracks = graphene.List(TrackType)
@@ -32,10 +34,37 @@ class CreateTrack(graphene.Mutation):
         title = kwargs.get('title')
         description = kwargs.get('description')
         url = kwargs.get('url')
-        track = Track(title=title, description=description, url=url, posted_by=user)
+        track = Track(title=title, description=description,
+                      url=url, posted_by=user)
         track.save()
         return CreateTrack(track=track)
 
 
+class UpdateTrack(graphene.Mutation):
+    track = graphene.Field(TrackType)
+
+    class Arguments:
+        track_id = graphene.Int(required=True)
+        title = graphene.String()
+        description = graphene.String()
+        url = graphene.String()
+
+    def mutate(self, info, track_id, title, url, description):
+        user = info.context.user
+        track = Track.objects.get(id=track_id)
+
+        if track.posted_by != user:
+            raise Exception('Not permitted to update this track')
+
+        track.title = title
+        track.description = description
+        track.url = url
+
+        track.save()
+
+        return UpdateTrack(track=track)
+
+
 class Mutation(graphene.ObjectType):
     create_track = CreateTrack.Field()
+    update_track = UpdateTrack.Field()
