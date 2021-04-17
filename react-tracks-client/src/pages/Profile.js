79b65c9f6131b211596941065fd5f0a91960f6d0
goughjo02 <goughjo02@gmail.com>
2021-04-17 21:34:15 +0100

    add profile query component

--- react-tracks-client/src/pages/Profile.js
+++ react-tracks-client/src/pages/Profile.js
@@ -1,19 +1,113 @@
 import React from "react";
+import { Query } from "react-apollo";
+import { gql } from "apollo-boost";
 import withStyles from "@material-ui/core/styles/withStyles";
-// import Card from "@material-ui/core/Card";
-// import CardHeader from "@material-ui/core/CardHeader";
-// import Avatar from "@material-ui/core/Avatar";
-// import Paper from "@material-ui/core/Paper";
-// import Typography from "@material-ui/core/Typography";
-// import ThumbUpIcon from "@material-ui/icons/ThumbUpTwoTone";
-// import AudiotrackIcon from "@material-ui/icons/AudiotrackTwoTone";
-// import Divider from "@material-ui/core/Divider";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import Avatar from "@material-ui/core/Avatar";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import ThumbUpIcon from "@material-ui/icons/ThumbUpTwoTone";
+import AudiotrackIcon from "@material-ui/icons/AudiotrackTwoTone";
+import Divider from "@material-ui/core/Divider";
+import AudioPlayer from "../components/Shared/AudioPlayer";
+import Loading from "../components/Shared/Loading";
+import Error from "../components/Shared/Error";
 
-const Profile = ({ classes }) => {
-  return <div>Profile</div>;
+const Profile = ({ classes, match }) => {
+  const id = Number(match.params.id);
+  return (
+    <Query query={PROFILE_QUERY} variables={{ id }}>
+      {({ data, loading, error }) => {
+        if (loading) return <Loading />;
+        if (error) return <Error error={error} />;
+
+        return (
+          <div>
+            {/* User info card */}
+            <Card className={classes.card}>
+              <CardHeader
+                avatar={<Avatar>{data.user.username[0]}</Avatar>}
+                title={data.user.username}
+                subheader={`Joined ${data.user.dateJoined}`}
+              />
+            </Card>
+
+            {/* Created Tracks */}
+            <Paper elevation={1} className={classes.paper}>
+              <Typography variant="title" className={classes.title}>
+                <AudiotrackIcon className={classes.audioIcon} />
+                Created Tracks
+              </Typography>
+              {data.user.trackSet.map((track) => (
+                <div key={track.id}>
+                  <Typography>
+                    {track.title} {track.likes.length} likes
+                  </Typography>
+                  <AudioPlayer url={track.url} />
+                  <Divider className={classes.divider} />
+                </div>
+              ))}
+            </Paper>
+
+            {/* Liked Tracks */}
+            <Paper elevation={1} className={classes.paper}>
+              <Typography variant="title" className={classes.title}>
+                <ThumbUpIcon className={classes.thumbIcon} />
+                Liked Tracks
+              </Typography>
+              {data.user.likeSet.map(({ track }) => (
+                <div key={track.id}>
+                  <Typography>
+                    {track.title} {track.likes.length} likes
+                    {track.postedBy.username}
+                  </Typography>
+                  <AudioPlayer url={track.url} />
+                  <Divider className={classes.divider} />
+                </div>
+              ))}
+            </Paper>
+          </div>
+        );
+      }}
+    </Query>
+  );
 };
 
-const styles = theme => ({
+const PROFILE_QUERY = gql`
+  query($id: Int!) {
+    user(id: $id) {
+      id
+      username
+      dateJoined
+      likeSet {
+        id
+        track {
+          id
+          title
+          url
+          likes {
+            id
+          }
+          postedBy {
+            id
+            username
+          }
+        }
+      }
+      trackSet {
+        id
+        title
+        url
+        likes {
+          id
+        }
+      }
+    }
+  }
+`;
+
+const styles = (theme) => ({
   paper: {
     width: "auto",
     display: "block",
@@ -25,31 +119,31 @@ const styles = theme => ({
     [theme.breakpoints.up("md")]: {
       width: 650,
       marginLeft: "auto",
-      marginRight: "auto"
-    }
+      marginRight: "auto",
+    },
   },
   card: {
     display: "flex",
-    justifyContent: "center"
+    justifyContent: "center",
   },
   title: {
     display: "flex",
     alignItems: "center",
-    marginBottom: theme.spacing.unit * 2
+    marginBottom: theme.spacing.unit * 2,
   },
   audioIcon: {
     color: "purple",
     fontSize: 30,
-    marginRight: theme.spacing.unit
+    marginRight: theme.spacing.unit,
   },
   thumbIcon: {
     color: "green",
-    marginRight: theme.spacing.unit
+    marginRight: theme.spacing.unit,
   },
   divider: {
     marginTop: theme.spacing.unit,
-    marginBottom: theme.spacing.unit
-  }
+    marginBottom: theme.spacing.unit,
+  },
 });
 
 export default withStyles(styles)(Profile);
