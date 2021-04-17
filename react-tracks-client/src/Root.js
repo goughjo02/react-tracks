854ba5f1c83fda634ae01225b63b1388ad17c67b
goughjo02 <goughjo02@gmail.com>
2021-04-17 14:39:18 +0100

    add routes

--- react-tracks-client/src/Root.js
+++ react-tracks-client/src/Root.js
@@ -1,39 +1,52 @@
 import React from "react";
-import withRoot from "./withRoot";
-
 import { Query } from "react-apollo";
 import { gql } from "apollo-boost";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import withRoot from "./withRoot";
+import App from "./pages/App";
+import Profile from "./pages/Profile";
+import Header from "./components/Shared/Header";
 
 const Root = () => (
-    <Query query={ME_QUERY}>
-        {({ data, loading, error }) => {
-            if (loading) return <div>loading0</div>
-            if (error) return <div>Error</div>
+  <Query query={ME_QUERY}>
+    {({ data, loading, error }) => {
+      if (loading) return <div>loading</div>;
+      if (error) return <div>Error</div>;
 
-            return <div>{JSON.stringify(data)}</div>
-        }}
-    </Query>
-)
+      return (
+        <Router>
+          <>
+            <Header />
+            <Switch>
+              <Route exact path="/" component={App} />
+              <Route path="/profile/:id" component={Profile} />
+            </Switch>
+          </>
+        </Router>
+      );
+    }}
+  </Query>
+);
 
 const ME_QUERY = gql`
-    {
-        me {
-            id
-            username
-            email
-        }
+  {
+    me {
+      id
+      username
+      email
     }
-`
+  }
+`;
 
 const GET_TRACKS_QUERY = gql`
-    {
-        tracks {
-            id
-            title
-            description
-            url
-        }
+  {
+    tracks {
+      id
+      title
+      description
+      url
     }
-`
+  }
+`;
 
 export default withRoot(Root);
